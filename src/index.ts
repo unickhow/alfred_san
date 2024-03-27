a6430759b7fb29db1df44af0aeb7acc2977218c3
unickhow <unick.zhow@gmail.com>
2024-03-27 23:45:56 +0800

    💡 feat: setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
-import { Elysia } from "elysia";
+import { Elysia } from "elysia"
+import line from '@line/bot-sdk'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+}
+
+const client = new line.Client(config)
+const app = new Elysia()
+
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
+})
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null)
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text }
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo)
+}
+
+app.get('/', () => "Hello Elysia")
+
+app.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
